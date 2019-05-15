a47cd05e6278c161d869ab9d92616551d18ffee1
Rahmicc <rahmi_cc@yahoo.com>
2019-05-15 07:32:51 +0000

    This is first commit Signed-off-by: Rahmicc <rahmi_cc@yahoo.com>

---
+++ GitFirst/src/com/turkcell/gitfirst/Demo1.java
@@ -0,0 +1,11 @@
+package com.turkcell.gitfirst;
+
+public class Demo1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("This is first program that uses git in eclipse");
+
+	}
+
+}
