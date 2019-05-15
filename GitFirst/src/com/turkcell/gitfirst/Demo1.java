000c84de0839c89831643e64a7b77ab35ee56095
Rahmicc <rahmi_cc@yahoo.com>
2019-05-15 07:35:12 +0000

    second one

--- GitFirst/src/com/turkcell/gitfirst/Demo1.java
+++ GitFirst/src/com/turkcell/gitfirst/Demo1.java
@@ -4,7 +4,7 @@ public class Demo1 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("This is first program that uses git in eclipse");
+		System.out.println("This is second program that uses git in eclipse");
 
 	}
 
